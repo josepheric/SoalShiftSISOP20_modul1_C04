76b88b73dc04158984c1f792734d0364750f3a0f
josepheric <61129358+josepheric@users.noreply.github.com>
2020-02-21 17:00:03 +0700

    Update soal1a.sh

--- soal1a.sh
+++ soal1a.sh
@@ -2,12 +2,11 @@ awk -F"," 'BEGIN {
 min = 9007199254740992; #very large number
 }
 {
-dikit[$13]++;
 count[$13]+= $21;
 }
 END{ 
   for (i in count)
-	{	if (count[i] == 0)
+	{	if (i == "Region")
 		{min = min}
 		else  if (count[i] < min)
 		{ min = count[i];
