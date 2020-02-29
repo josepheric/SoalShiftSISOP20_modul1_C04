de98ccc4ef8af1b01ac2f9d20a5a1ab4de400d1e
josepheric <61129358+josepheric@users.noreply.github.com>
2020-02-29 20:32:49 +0700

    Update soal2d.sh

--- Soal2/soal2d.sh
+++ Soal2/soal2d.sh
@@ -1,34 +1,36 @@
-cp /var/log/syslog backup
 
-a=$(date -r $1 "%H")
+read -p "Enter nama File yang di decrypt" N
+cp /home/dennas/$N.txt backup
+a=$(date +"%H")
 case "$a" in
-	"01" ) cat backup | tr '[a-z]' '[B-ZA-A]' ;; 
-        "02" ) cat backup | tr '[a-z]' '[C-ZA-B]' ;; 
-        "03" ) cat backup | tr '[a-z]' '[D-ZA-C]' ;; 
-        "04" ) cat backup | tr '[a-z]' '[E-ZA-D]' ;; 
-        "05" ) cat backup | tr '[a-z]' '[F-ZA-E]' ;; 
-        "06" ) cat backup | tr '[a-z]' '[G-ZA-F]' ;; 
-        "07" ) cat backup | tr '[a-z]' '[H-ZA-G]' ;; 
-        "08" ) cat backup | tr '[a-z]' '[I-ZA-H]' ;;
-        "09" ) cat backup | tr '[a-z]' '[J-ZA-I]' ;;
-        "10" ) cat backup | tr '[a-z]' '[K-ZA-J]' ;; 
-        "11" ) cat backup | tr '[a-z]' '[L-ZA-K]' ;; 
-        "12" ) cat backup | tr '[a-z]' '[M-ZA-L]' ;; 
-        "13" ) cat backup | tr '[a-z]' '[N-ZA-M]' ;; 
-        "14" ) cat backup | tr '[a-z]' '[O-ZA-N]' ;; 
-        "15" ) cat backup | tr '[a-z]' '[P-ZA-O]' ;; 
-        "16" ) cat backup | tr '[a-z]' '[Q-ZA-P]' ;; 
-        "17" ) cat backup | tr '[a-z]' '[R-ZA-Q]' ;; 
-        "18" ) cat backup | tr '[a-z]' '[S-ZA-R]' ;; 
-        "19" ) cat backup | tr '[a-z]' '[T-ZA-S]' ;; 
-        "20" ) cat backup | tr '[a-z]' '[U-ZA-T]' ;; 
-        "21" ) cat backup | tr '[a-z]' '[V-ZA-U]' ;; 
-        "22" ) cat backup | tr '[a-z]' '[W-ZA-V]' ;; 
-        "23" ) cat backup | tr '[a-z]' '[X-ZA-W]' ;; 
-        "00" ) cat backup | tr '[a-z]' '[Y-ZA-X]' ;; 
+	"01" ) cat backup | tr  '[B-ZA-A]' '[a-z]'>File1.txt;; 
+        "02" ) cat backup | tr  '[C-ZA-B]'  '[a-z]' >File1.txt;; 
+        "03" ) cat backup | tr  '[D-ZA-C]' '[a-z]'>File1.txt;; 
+        "04" ) cat backup | tr  '[E-ZA-D]' '[a-z]' | tr '[E-ZA-D]' '[A-Z]' >File1;;
+        "05" ) cat backup | tr  '[F-ZA-E]' '[a-z]' >File1.txt;; 
+        "06" ) cat backup | tr  '[G-ZA-F]' '[a-z]' >File1.txt;; 
+        "07" ) cat backup | tr  '[H-ZA-G]' '[a-z]' >File1.txt ;; 
+        "08" ) cat backup | tr  '[I-ZA-H]' '[a-z]'>File1.txt;;
+        "09" ) cat backup | tr  '[J-ZA-I]'  '[a-z]'>File1.txt;;
+        "10" ) cat backup | tr  '[K-ZA-J]'  '[a-z]' >File1.txt;; 
+        "11" ) cat backup | tr  '[L-ZA-K]' '[a-z]'>File1.txt;; 
+        "12" ) cat backup | tr '[M-ZA-L]''[a-z]'  >File1.txt;; 
+        "13" ) cat backup | tr '[N-ZA-M]' '[a-z]'  >File1.txt;; 
+        "14" ) cat backup | tr '[O-ZA-N]''[a-z]' >File1.txt;; 
+        "15" ) cat backup | tr  '[P-ZA-O]' '[a-z]' >File1.txt;; 
+        "16" ) cat backup | tr  '[Q-ZA-P]' '[a-z]' >File1.txt;; 
+        "17" ) cat backup | tr '[R-ZA-Q]' '[a-z]'  >File1.txt;; 
+        "18" ) cat backup | tr  '[S-ZA-R]' '[a-z]' >File1.txt;; 
+        "19" ) cat backup | tr  '[T-ZA-S]' '[a-z]' >File1.txt;; 
+        "20" ) cat backup | tr  '[U-ZA-T]'  '[a-z]'>File1.txt;; 
+        "21" ) cat backup | tr  '[V-ZA-U]' '[a-z]' >File1.txt;; 
+        "22" ) cat backup | tr  '[W-ZA-V]' '[a-z]'>File1.txt;; 
+        "23" ) cat backup | tr  '[X-ZA-W]' '[a-z]' >File1.txt;; 
+        "00" ) cat backup | tr  '[Y-ZA-X]' '[a-z]' >File1.txt;; 
 
 
 
 
 esac
-mv  backup  $(date +"%H:%M_%d-%m-%y")
+
+mv  $N.txt  File3.txt
